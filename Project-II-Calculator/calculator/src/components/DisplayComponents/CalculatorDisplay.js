ed39387f6e65268ce7a3986069c351e5935de3a9
Katie Rose <katiefelten@gmail.com>
2019-05-28 18:27:16 -0400

    Calculator complete

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return (
+    <div className="output-display">
+      <div className="number-display">{props.number}</div>
+        <div className="operators"></div>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
